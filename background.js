c7181baaadcc8f3a3a796485434573928cf9b21e
melodykramer <melodykramer@gmail.com>
2014-02-03 20:16:15 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,12 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(function(info) {
 
-  var url = info.url.split('?')[0];
+  var url = info.url;
 
-  // if(url.indexOf('?') !== -1) {
-  //   url += '&share=1';
-  // } else {
-  //   url += '?share=1';
-  // }
 
   return {
     redirectUrl: url + 'utm_medium=pinterest&utm_source=nprfood&utm_campaign=arts'
